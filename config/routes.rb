d78510734e97dcc7e42a63591610a6462c161e95
Kerollos Magdy <kerolloz@yahoo.com>
2019-08-19 14:55:51 +0200

    root redirect to welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome', to: 'welcome#index'
+  root 'welcome#index'
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
